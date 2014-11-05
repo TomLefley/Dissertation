bdbfa7be12326f827f5096978b2dd6f6ba23466d
TomLefley <thomas.lefley@gmail.com>
2014-11-05 20:31:46 +0000

    Threads print their id

--- Unity/Assets/Scripts/DirectCompute/KernelExample.cs
+++ Unity/Assets/Scripts/DirectCompute/KernelExample.cs
@@ -6,7 +6,20 @@ public class KernelExample : MonoBehaviour {
     public ComputeShader shader;
 
     void Start() {
-        shader.Dispatch(0, 1, 1, 1);
+        ComputeBuffer buffer = new ComputeBuffer(4 * 2, sizeof(int));
+
+        shader.SetBuffer(0, "buffer1", buffer);
+
+        shader.Dispatch(0, 2, 1, 1);
+
+        int[] data = new int[4 * 2];
+
+        buffer.GetData(data);
+
+        for (int i = 0; i < 4 * 2; i++)
+            Debug.Log(data[i]);
+
+        buffer.Release();
     }
 
 }
