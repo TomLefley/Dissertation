8b5aee40f802cc37617a6bf76e9086a1097a36eb
TomLefley <thomas.lefley@gmail.com>
2014-11-05 20:49:22 +0000

    Buffer now takes a custom struct

--- Unity/Assets/Scripts/DirectCompute/BufferExample.cs
+++ Unity/Assets/Scripts/DirectCompute/BufferExample.cs
@@ -9,20 +9,34 @@ public class BufferExample : MonoBehaviour {
     const int count = 1024;
     const float size = 5.0f;
 
+    struct Vert {
+        public Vector3 position;
+        public Vector3 color;
+    }
+
     void Start() {
 
-        buffer = new ComputeBuffer(count, sizeof(float) * 3, ComputeBufferType.Default);
+        buffer = new ComputeBuffer(count, sizeof(float) * 6, ComputeBufferType.Default);
 
-        float[] points = new float[count * 3];
+        Vert[] points = new Vert[count];
 
         Random.seed = 0;
         for (int i = 0; i < count; i++) {
-            points[i * 3 + 0] = Random.Range(-size, size);
-            points[i * 3 + 1] = Random.Range(-size, size);
-            points[i * 3 + 2] = 0.0f;
+            points[i] = new Vert();
+
+            points[i].position = new Vector3();
+            points[i].position.x = Random.Range(-size, size);
+            points[i].position.y = Random.Range(-size, size);
+            points[i].position.z = 0;
+
+            points[i].color = new Vector3();
+            points[i].color.x = Random.value > 0.5f ? 0.0f : 1.0f;
+            points[i].color.y = Random.value > 0.5f ? 0.0f : 1.0f;
+            points[i].color.z = Random.value > 0.5f ? 0.0f : 1.0f;
         }
 
         buffer.SetData(points);
+
     }
 
     void OnPostRender() {
--- Unity/Assets/Shaders/DirectCompute/BufferShader.shader
+++ Unity/Assets/Shaders/DirectCompute/BufferShader.shader
@@ -1,8 +1,9 @@
-﻿Shader "Custom/BufferShader" {
-	SubShader
+﻿Shader "Custom/BufferShader"
+{
+    SubShader
     {
-       Pass
-       {
+        Pass
+        {
             ZTest Always Cull Off ZWrite Off
             Fog { Mode off }
  
@@ -12,28 +13,37 @@
             #pragma vertex vert
             #pragma fragment frag
  
-            uniform StructuredBuffer<float3> buffer;
+            struct Vert
+            {
+                float3 position;
+                float3 color;
+            };
+ 
+            uniform StructuredBuffer<Vert> buffer;
  
             struct v2f
             {
                 float4  pos : SV_POSITION;
+                float3 col : COLOR;
             };
  
             v2f vert(uint id : SV_VertexID)
             {
-                float4 pos = float4(buffer[id], 1);
+                Vert vert = buffer[id];
  
                 v2f OUT;
-                OUT.pos = mul(UNITY_MATRIX_MVP, pos);
+                OUT.pos = mul(UNITY_MATRIX_MVP, float4(vert.position, 1));
+                OUT.col = vert.color;
                 return OUT;
             }
  
             float4 frag(v2f IN) : COLOR
             {
-                return float4(1,0,0,1);
+                return float4(IN.col,1);
             }
  
             ENDCG
+ 
         }
     }
 }
