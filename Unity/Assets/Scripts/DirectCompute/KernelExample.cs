681b308426e9b50a5593f0378cd4f19b74c6e32d
TomLefley <thomas.lefley@gmail.com>
2014-11-05 20:35:34 +0000

    2D kernels

--- Unity/Assets/Scripts/DirectCompute/KernelExample.cs
+++ Unity/Assets/Scripts/DirectCompute/KernelExample.cs
@@ -6,18 +6,26 @@ public class KernelExample : MonoBehaviour {
     public ComputeShader shader;
 
     void Start() {
-        ComputeBuffer buffer = new ComputeBuffer(4 * 2, sizeof(int));
 
-        shader.SetBuffer(0, "buffer1", buffer);
+        ComputeBuffer buffer = new ComputeBuffer(4 * 4 * 2 * 2, sizeof(int));
 
-        shader.Dispatch(0, 2, 1, 1);
+        int kernel = shader.FindKernel("CSMain2");
 
-        int[] data = new int[4 * 2];
+        shader.SetBuffer(kernel, "buffer2", buffer);
+
+        shader.Dispatch(kernel, 2, 2, 1);
+
+        int[] data = new int[4 * 4 * 2 * 2];
 
         buffer.GetData(data);
 
-        for (int i = 0; i < 4 * 2; i++)
-            Debug.Log(data[i]);
+        for (int i = 0; i < 8; i++) {
+            string line = "";
+            for (int j = 0; j < 8; j++) {
+                line += " " + data[j + i * 8];
+            }
+            Debug.Log(line);
+        }
 
         buffer.Release();
     }
