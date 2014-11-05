2b22bc4e8dd17dc05122e1ff70a547b16a6b892e
TomLefley <thomas.lefley@gmail.com>
2014-11-05 20:27:17 +0000

    Created blank kernel

---
+++ Unity/Assets/Scripts/DirectCompute/KernelExample.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KernelExample : MonoBehaviour {
+
+    public ComputeShader shader;
+
+    void Start() {
+        shader.Dispatch(0, 1, 1, 1);
+    }
+
+}
