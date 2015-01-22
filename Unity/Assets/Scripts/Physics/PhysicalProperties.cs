5ac058064b8f5be608a89f17e3c9f7fd81b4253d
TomLefley <thomas.lefley@gmail.com>
2015-01-22 13:59:04 +0000

    Updated physics

--- Unity/Assets/Scripts/Physics/PhysicalProperties.cs
+++ Unity/Assets/Scripts/Physics/PhysicalProperties.cs
@@ -3,22 +3,9 @@ using System.Collections;
 
 public class PhysicalProperties : MonoBehaviour {
 
-    public static float threshold = 0;
-
     public float brittleness;
-    public int mass;
+    public float mass;
 
     private float acceleration;
 
-    public float doesBreak(float momentum) {
-        return Mathf.Max(0f, (brittleness * momentum) - threshold);
-    }
-
-    public Vector3 getVelocity() {
-        return gameObject.rigidbody.velocity;
-    }
-
-    public float getMomentum() {
-        return getVelocity().magnitude * mass;
-    }
 }
--- Unity/Assets/Scripts/Voxelisation/VoxelisationTrigger.cs
+++ Unity/Assets/Scripts/Voxelisation/VoxelisationTrigger.cs
@@ -24,6 +24,36 @@ public class VoxelisationTrigger : MonoBehaviour {
 
         float radius = gameObjectToVoxelisePP.doesBreak(collidingPP.getMomentum());
 
+        RaycastHit willCollidePoint;
+
+        Ray ray = new Ray(collider.transform.position, collider.rigidbody.velocity);
+
+        Physics.Raycast(ray, out willCollidePoint);
+
+        Rigidbody willCollideWith = willCollidePoint.rigidbody;
+
+        Vector3 velocity1 = willCollideWith ? willCollideWith.velocity : Vector3.zero;
+
+        Vector3 velocity2 = collider.rigidbody.velocity;
+
+        float mass1 = gameObjectToVoxelisePP.mass;
+
+        Debug.Log(mass1);
+
+        float mass2 = collider.rigidbody.mass;
+
+        Debug.Log(mass2);
+
+        Vector3 willCollideNormal = willCollidePoint.normal;
+        willCollideNormal.Normalize();
+        willCollideNormal*=2;
+
+        float exertedForce = Vector3.Dot(willCollideNormal,(mass2 * velocity1 - mass1 * velocity2)) / (mass1 + mass2);
+
+        exertedForce /= Time.fixedDeltaTime;
+
+        Debug.Log(exertedForce);
+
         //if (radius == 0f) return;
 
         Debug.Log(radius);
